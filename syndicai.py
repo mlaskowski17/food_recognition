f9e7dc8c1b9fd8634b0baca92d8f55a23c19ed98
mlaskowski17 <marcin.ladesign@gmail.com>
2020-10-18 12:25:44 +0200

    model run - bug fix

--- syndicai.py
+++ syndicai.py
@@ -1,3 +1,4 @@
+import os
 import requests
 import numpy as np
 from PIL import Image
@@ -13,10 +14,11 @@ labels_path = "labels.txt"
 
 class syndicai(object):
     def __init__(self):
-        download_model(model_url)
-        self.model = load_model(model_path, compile=False)
+        if not model_path in os.listdir():
+            download_model(model_url)
 
     def predict(self, url, features_names=None):
+        print(url)
         image = requests.get(url).content
         # adjust the image size to the size of images in the train dataset
         image_preprocessed = np.array(Image.open(BytesIO(image)).resize((299, 299))).astype(np.float) / 255
@@ -25,9 +27,7 @@ class syndicai(object):
         with open(labels_path) as file:
             labels = file.read()
         labels = labels.split(',')
-        predictions = self.model.predict(image_preprocessed)
+        model = load_model(model_path, compile=False)
+        predictions = model.predict(image_preprocessed)
         index = np.argmax(predictions)
         return labels[index]
-
-    def metrics(self):
-        return 'Not Implemented'
